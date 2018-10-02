666cd478ccce68dabc4bb1747082c3779e2684cc
mary griboedova <mary.griboedova@gmail.com>
2018-10-02 23:35:02 +0300

    Decomposed classes for group tests

--- fixture/application.py
+++ fixture/application.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
+from fixture.group import GroupHelper
+from fixture.session import SessionHelper
+
 
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def add_address_contact(self, contact, date, date2):
         self.init_contact_creation()
@@ -136,44 +137,13 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_main_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("%s" % username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("%s" % password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
     def open_main_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
-    def return_to_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        #  fill in group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("%s" % group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("%s" % group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
+    # def return_to_group_page(self):
+    #     wd = self.wd
+    #     wd.find_element_by_link_text("group page").click()
 
     def destroy(self):
         wd = self.wd
